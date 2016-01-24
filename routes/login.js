b03052404c7802d374411d24872e938cb6ca5ddc
Peach <scdzwyxst@gmail.com>
2016-01-24 22:39:22 +0800

    Add activation feature in backend

--- routes/login.js
+++ routes/login.js
@@ -1,5 +1,6 @@
 var express = require('express'),
-    router = express.Router();
+    router = express.Router(),
+    models = require('../models');
 
 router.get('/login', function (req, res) {
   res.render('login.html', { message: req.flash('error') });
@@ -16,6 +17,45 @@ router.post('/login',
 router.get('/logout', require('../helpers/auth_check'), function (req, res) {
   req.logout();
   res.redirect('/login');
+});
+
+router.post('/activation', function (req, res) {
+  var data = req.body;
+
+  if(data.id){
+    models.User.findOne({
+      where: {
+        id: data.id
+      }
+    }).then(function (user) {
+      if(user){
+        if(user.get('password')){
+          req.flash('info', '该账号已激活，不可重复激活！');
+          res.redirect('/login');
+        }else if(user.get('name') == data.name){
+          models.User.update({
+            mobile: data.mobile,
+            email: data.email,
+            password: data.password
+          }, {
+            where: {
+              id: data.id
+            }
+          }).then(function () {
+            req.flash('info', '激活账号成功！');
+            res.redirect('/login');
+          });
+        }else{
+          req.flash('act_error', '输入的真实姓名有误！');
+          res.redirect('/login');
+        }
+      }else{
+        res.redirect('/login');
+      }
+    })
+  }else{
+    res.redirect('/login');
+  }
 });
 
 module.exports = router;
